7e7724f9c9c735979c7cd48e4560d67ca8636dba
Xuan Huy <xuanhuy900@gmail.com>
2020-12-09 16:06:36 +0700

    first commit

---
+++ Building-Manage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Building_Manage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
